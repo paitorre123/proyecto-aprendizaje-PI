6672d87665fdb039f35fa65209a28f98361d02ce
paitorre123 <paitorre@alumnos.ubiobio.cl>
2020-08-03 12:26:24 -0400

    manejo de datos a traves de archivo externo

--- proyecto-aprendizaje-PI/src/cl/empleados/DirectorEmpleado.java
+++ proyecto-aprendizaje-PI/src/cl/empleados/DirectorEmpleado.java
@@ -3,6 +3,8 @@ package cl.empleados;
 public class DirectorEmpleado  implements IEmpleado{
 	
 	private IUsableInformes informeNuevo;
+	private String email;
+	private String nombreEmpresa;
 	
 	//Dependencia del informe
 	public DirectorEmpleado(IUsableInformes informeNuevo) {
@@ -21,4 +23,21 @@ public class DirectorEmpleado  implements IEmpleado{
 		return "Informe creado por el Director:"+ this.informeNuevo.getInforme();
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+
+	public void setNombreEmpresa(String nombreEmpresa) {
+		this.nombreEmpresa = nombreEmpresa;
+	}
+	
+	
 }
--- proyecto-aprendizaje-PI/src/cl/empleados/UsoEmpleado.java
+++ proyecto-aprendizaje-PI/src/cl/empleados/UsoEmpleado.java
@@ -8,12 +8,14 @@ public class UsoEmpleado {
 		//1. cargar archivo xml
 		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
 		//2.Pedir el Bean
-		IEmpleado e1 = contexto.getBean("miEmpleado", IEmpleado.class);
+		DirectorEmpleado e1 = contexto.getBean("miEmpleado", DirectorEmpleado.class);
 		SecretarioEmpleado e2 = contexto.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
 		//3.Uso del contenedor
 		System.out.println("EMPLEADO: "+ e1);
 		System.out.println(e1.getTarea());
 		System.out.println(e1.getInformes());
+		System.out.println(e1.getEmail());
+		System.out.println("Empresa: "+e1.getNombreEmpresa());
 		
 		System.out.println("\n");
 		
@@ -21,7 +23,8 @@ public class UsoEmpleado {
 		System.out.println(e2.getTarea());
 		System.out.println(e2.getInformes());
 		System.out.println(e2.getEmail());
-		System.out.println(e2.getNombreEmpresa());
+		System.out.println("Empresa: "+e2.getNombreEmpresa());
+		
 		//4.Cerrar el contexto (liberar memoria)
 		contexto.close();
 	}
