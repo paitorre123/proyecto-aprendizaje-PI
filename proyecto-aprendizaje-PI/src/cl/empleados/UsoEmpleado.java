047ec53f3ca1e7ca9b602890ea2871de50b4533b
paitorre123 <paitorre@alumnos.ubiobio.cl>
2020-08-03 12:52:29 -0400

    uso del patron (scope en applicationContext) singleton y prototype

--- proyecto-aprendizaje-PI/src/cl/empleados/UsoEmpleado.java
+++ proyecto-aprendizaje-PI/src/cl/empleados/UsoEmpleado.java
@@ -7,23 +7,28 @@ public class UsoEmpleado {
 	public static void main(String[] args) {
 		//1. cargar archivo xml
 		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
-		//2.Pedir el Bean
+		//2.Pedir el Bean al contenedor de spring
 		DirectorEmpleado e1 = contexto.getBean("miEmpleado", DirectorEmpleado.class);
-		SecretarioEmpleado e2 = contexto.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
+		SecretarioEmpleado se1= contexto.getBean("miSecretarioEmpleadoPrototype", SecretarioEmpleado.class);
+		SecretarioEmpleado se2= contexto.getBean("miSecretarioEmpleadoPrototype", SecretarioEmpleado.class);
 		//3.Uso del contenedor
-		System.out.println("EMPLEADO: "+ e1);
-		System.out.println(e1.getTarea());
-		System.out.println(e1.getInformes());
-		System.out.println(e1.getEmail());
-		System.out.println("Empresa: "+e1.getNombreEmpresa());
+		System.out.println("EMPLEADO DIRECTIVO: "+ e1);
+//		System.out.println(e1.getTarea());
+//		System.out.println(e1.getInformes());
+//		System.out.println(e1.getEmail());
+//		System.out.println("Empresa: "+e1.getNombreEmpresa());
 		
 		System.out.println("\n");
 		
-		System.out.println("EMPLEADO: "+ e2);
-		System.out.println(e2.getTarea());
-		System.out.println(e2.getInformes());
-		System.out.println(e2.getEmail());
-		System.out.println("Empresa: "+e2.getNombreEmpresa());
+		System.out.println("EMPLEADO SECRETARIO 1: "+ se1);
+		
+		System.out.println("\n");
+		
+		System.out.println("EMPLEADO SECRETARIO 2: "+ se2);
+//		System.out.println(se1.getTarea());
+//		System.out.println(se1.getInformes());
+//		System.out.println(se1.getEmail());
+//		System.out.println("Empresa: "+se1.getNombreEmpresa());
 		
 		//4.Cerrar el contexto (liberar memoria)
 		contexto.close();
